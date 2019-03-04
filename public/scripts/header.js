f5c8a343614c5568b87b30e73a261d4fbe90d458
Alexis Louie <alouie1020@gmail.com>
2019-03-03 22:25:00 -0500

    removed unauthroized error alert

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,16 +1,8 @@
-if (!bearer) {
-    $('main').html(`
-        <div class="unauthorized">
-            <b>ERROR 404</b> unauthorized<br />
-            <span class="login">Please <a href="/login.html">log in to view</a></span>
-        </div>
-    `);
-}
+// const bearer = localStorage.authToken;
 
 if (bearer) {
-    $('header ul').html('<li>Log Out</li>');
-    $('header').on('click', 'li', function(event) {
-        // event.preventDefault();
+    $('header ul').html('<a href="/logout.html">Log Out</a>');
+    $('header a').click(function(event) {
         window.location.replace('/index.html');
         localStorage.removeItem('authToken');
         localStorage.removeItem('userId');

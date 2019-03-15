22d3dd9738b715478b84c2bfdee48b7a200ae966
Alexis Louie <alouie1020@gmail.com>
2019-03-15 00:36:12 -0400

    fixed bug when toggling and rendering programs

--- public/scripts/create-program.js
+++ public/scripts/create-program.js
@@ -12,16 +12,20 @@ if (!bearer) {
     `);
 }
 
-function addProgLength() {
-    $('body').on('click', '.js-add-progLength', function (event) {
-        event.preventDefault();
+
+$('body').on('click', '.js-add-progLength', function (event) {
+    event.preventDefault();
+    $('.errors').remove();
+
+    const dayCount = $('.programLength').val();
+    if (!dayCount || dayCount.trim().length < 0 || dayCount <= 0) {
+        $('.form-header').after(`<div class="errors">Please enter valid number for Program Length</div>`)
+    } else {
         $('.js-add-program-length').hide();
-        const dayCount = $('.programLength').val();
         $('.js-add-program-form').append(`
-            <br />
-            <br />
-            Schedule:
-        `);
+                <br />
+                <label>SCHEDULE:</label><br />
+            `);
 
         let html = ''
         for (let i = 0; i < dayCount; i++) {
@@ -31,41 +35,43 @@ function addProgLength() {
 
         function renderSingleDay() {
             return `
-            <div class="day">
-                <div class="exercises">
-                    <button class="js-add-exercise">Add Exercise</button>
-                </div>
-            </div>
-          `
+                    <div class="day">
+                        <div class="exercises">
+                            <button class="js-add-exercise add-exercise-button">ADD EXERCISE</button>
+                        </div>
+                    </div>
+                `
         }
 
         function renderMultiDay(day) {
             return `
-            <fieldset>
-              <legend>Day ${day}</legend>
-              <div class="day">
-                  <label for="name">Name:</label>
-                  <input type="text" class="name" placeholder="e.g. Day 1: Legs" required>
-                  <br />
-                  <div class="exercises">
-                      <button class="js-add-exercise multiday">Add Exercise</button>
-                  </div>
-              </div>
-            </fieldset>
-          `
+                    <fieldset>
+                        <legend>Day ${day}</legend>
+                        <div class="day">
+                            <label for="name">Name:</label>
+                            <input type="text" class="name" placeholder="e.g. Day 1: Legs" required>
+                            <br />
+                            <div class="exercises">
+                                <button class="js-add-exercise add-exercise-button multiday">ADD EXERCISE</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                `
         }
 
         $('.js-add-program-form').append(`
             <br />
-            <button type=submit class="js-save-program">Save Program</button>
+            <hr width=75% align="center" class="line-break">
+            <button type="submit" class="js-save-program save-program-button">SAVE PROGRAM</button>
         `)
 
         addExercise();
         $('.js-add-exercise').trigger('click');
 
         removeExercise();
-    })
-}
+    }
+})
+
 
 function addExercise() {
     let count = 0;
@@ -74,23 +80,22 @@ function addExercise() {
         return `
         <div class="exercise">
             <label>Exercise Name:</label>
-            <input type="text" class="exercise-name" id="exercise-${++count}" required>
+            <input type="text" class="exercise-name" id="exercise-${++count}" placeholder="e.g. barbell squat" required>
             <br /> 
 
             <label>Exercise Type:</label>
             <select name="exercise-type" class="exercise-type" required>
                 <option disabled selected value>-- select option --</option>
-                <option value="sets-reps">sets & reps</option>
-                <option value="reps-time">reps & time</option>
-                <option value="reps-distance">reps & distance</option>
-                <option value="distance-time">distance & time</option>
+                <option value="sets & reps">sets & reps</option>
+                <option value="reps & time">reps & time</option>
+                <option value="reps & distance">reps & distance</option>
+                <option value="distance & time">distance & time</option>
                 <option value="reps">reps</option>
                 <option value="distance">distance</option>
                 <option value="time">time</option>
             </select>
             <br />
-
-            <button class="js-remove-exercise">Remove this Exercise</button>
+            <button class="js-remove-exercise remove-button">X</button>
         </div>
       `
     }
@@ -172,16 +177,16 @@ function addExerciseDetails(menu, type) {
     $(menu).siblings('.exercise-details').remove();
     let html;
     switch (type) {
-        case 'sets-reps':
+        case 'sets & reps':
             html = getSetsReps();
             break;
-        case 'reps-time':
+        case 'reps & time':
             html = getRepsTime();
             break;
-        case 'reps-distance':
+        case 'reps & distance':
             html = getRepsDistance();
             break;
-        case 'distance-time':
+        case 'distance & time':
             html = getDistanceTime();
             break;
 
@@ -200,28 +205,28 @@ function addExerciseDetails(menu, type) {
     $(`<div class="exercise-details">
             ${html}
             <label for="comments">Comments:</label>
-            <input type="text" class="comments">
+            <input type="text" class="comments" placeholder="optional">
         </div>
     `).insertAfter(menu)
 }
 
 function getSetsReps() {
-    return `<br />
+    return `
         <label for="sets">Sets:</label>
-        <input type="number" min="0" max="100" class="sets" required><br /> 
+        <input type="number" onkeydown="return event.keyCode !== 69" min="0" max="100" class="sets" required><br /> 
 
         <label for="reps">Reps:</label>
-        <input type="number" min="0" max="999" class="reps" required><br />
+        <input type="number" onkeydown="return event.keyCode !== 69" min="0" max="999" class="reps" required><br />
     `
 }
 
 function getRepsTime() {
     return `<br />
         <label for="reps">Reps:</label>
-        <input type="number" min="0" max="999" class="reps" required><br /> 
+        <input type="number" onkeydown="return event.keyCode !== 69" min="0" max="999" class="reps" required><br /> 
 
         <label for="time">Time:</label>
-        <input type="number" min="0" max="999" class="time" required>
+        <input type="number" onkeydown="return event.keyCode !== 69" min="0" max="999" class="time" required>
 
         <select name="unitTime" class="unitTime" required>
             <option value="min">min</option>
@@ -234,10 +239,10 @@ function getRepsTime() {
 function getRepsDistance() {
     return `<br />
         <label for="reps">Reps:</label>
-        <input type="number" min="0" max="999" class="reps" required><br /> 
+        <input type="number" onkeydown="return event.keyCode !== 69" min="0" max="999" class="reps" required><br /> 
 
         <label for="dist">Distance:</label>
-        <input type="number" min="0" class="distance" required>
+        <input type="number" onkeydown="return event.keyCode !== 69" min="0" class="distance" required>
 
         <select name="unitLength" class="unitLength" required>
             <option value="m">m</option>
@@ -251,7 +256,7 @@ function getRepsDistance() {
 function getDistanceTime() {
     return `<br />
         <label for="dist">Distance:</label>
-        <input type="number" min="0" class="distance" required>
+        <input type="number" onkeydown="return event.keyCode !== 69" min="0" class="distance" required>
 
         <select name="unitLength" class="unitLength" required>
             <option value="m">m</option>
@@ -261,7 +266,7 @@ function getDistanceTime() {
         </select><br /> 
 
         <label for="time">Time:</label>
-        <input type="number" min="0" max="999" class="time" required>
+        <input type="number" onkeydown="return event.keyCode !== 69" min="0" max="999" class="time" required>
 
         <select name="unitTime" class="unitTime" required>
             <option value="min">min</option>
@@ -274,14 +279,14 @@ function getDistanceTime() {
 function getReps() {
     return `<br />
         <label for="reps">Reps:</label>
-        <input type="number" min="0" max="999" class="reps" required><br /> 
+        <input type="number" onkeydown="return event.keyCode !== 69" min="0" max="999" class="reps" required><br /> 
     `
 }
 
 function getDistance() {
     return `<br />
         <label for="dist">Distance:</label>
-        <input type="number" min="0" class="distance" required>
+        <input type="number" onkeydown="return event.keyCode !== 69" min="0" class="distance" required>
 
         <select name="unitLength" class="unitLength" required>
             <option value="m">m</option>
@@ -295,7 +300,7 @@ function getDistance() {
 function getTime() {
     return `<br />
         <label for="time">Time:</label>
-        <input type="number" min="0" max="999" class="time" required>
+        <input type="number" onkeydown="return event.keyCode !== 69" min="0" max="999" class="time" required>
 
         <select name="unitTime" class="unitTime" required>
             <option value="min">min</option>
@@ -305,46 +310,49 @@ function getTime() {
     `
 }
 
-function createProgramObj() {
-    $('main').on('click', '.js-save-program', function (event) {
+$('main').on('click', '.js-save-program', function (event) {
+    event.preventDefault();
+    alertArr = []
+
+    const programObj = {};
+    const programName = $('.programName').val();
+    if (!programName || programName.trim().length === 0) {
+        alertArr.push('program name')
+    } else {
+        programObj['programName'] = programName.trim();
+    }
+
+    const categories = $('.createProgCategories').val();
+    if (!categories.length) {
+        alertArr.push('at least one category')
+    } else {
+        programObj['categories'] = categories;
+    }
+
+    programObj['schedule'] = createSchedule();
+
+
+    let uniqueAlert = []
+    uniqueAlert = [...new Set(alertArr)];
+    $('.errors').remove();
+    if (uniqueAlert.length > 0) {
+        $('.form-header').after(`<div class="errors">Please include the following details:</div>`)
+        uniqueAlert.forEach(a => {
+            $('.errors').append(`<li>${a}</li>`)
+        })
+    } else {
+        saveProgram(programObj);
+    }
+})
+
+
+$('.add-program-form').on('keyup keypress', function (event) {
+    var keyCode = event.keyCode || event.which;
+    if (keyCode === 13) {
         event.preventDefault();
-        alertArr = []
-
-        const programObj = {};
-        const programName = $('.programName').val();
-        if (!programName || programName.trim().length === 0) {
-            alertArr.push('program name')
-            // $('.programName').after(`<br/ ><span style="color: red font-size: 10px"><i>Please enter valid program length</i></span>`)
-        } else {
-            programObj['programName'] = programName.trim();
-        }
-
-        const categories = $('.categories').val();
-        if (!categories.length) {
-            alertArr.push('at least one category')
-            // $('.programName').after(`<br/ ><span style="color: red font-size: 10px"><i>Please enter valid program length</i></span>`)
-        } else {
-            programObj['categories'] = categories;
-        }
-
-        programObj['schedule'] = createSchedule();
-
-        
-        let uniqueAlert = []
-        uniqueAlert = [...new Set(alertArr)];
-        if (uniqueAlert.length) {
-            $('.errors').remove();
-            $('main').prepend(`<div class="errors">Please include the following details:</div>`)
-
-            uniqueAlert.forEach(a => {
-                $('.errors').append(`<li>${a}</li>`)
-            })
-        } else {
-            console.log('programObj: ', programObj)
-        }
-
-    })
-}
+        return false;
+    }
+});
 
 function createSchedule() {
     const schedule = [];
@@ -375,33 +383,30 @@ function createExercises(day) {
         const exerciseId = $(exercise).find('div > .exercise-name').data('exercise-id');
         exerciseObj['exercise'] = exerciseId;
 
-        const exerciseName = $(exercise).find('div > .exercise-name').first().val();
-        if (!exerciseName || exerciseName.trim().length === 0) {
-            alertArr.push('a name for each exercise');
-        } else {
-            exerciseObj['name'] = exerciseName;
-        }
-
         const type = $(exercise).find('.exercise-type').first().val();
         if (!type) {
             alertArr.push('an exercise type for each exercise')
         } else {
             exerciseObj['type'] = type;
 
-            const typeDescriptors = type.split('-');
+            const typeDescriptors = type.split(' & ');
             const firstTypeDescrip = $(exercise).find(`.${typeDescriptors[0]}`).first().val();
+
             if (firstTypeDescrip && firstTypeDescrip.trim().length > 0 && firstTypeDescrip > 0) {
                 exerciseObj[`${typeDescriptors[0]}`] = $(exercise).find(`.${typeDescriptors[0]}`).first().val();
             } else {
                 alertArr.push(`a valid number for ${typeDescriptors[0]}`);
             }
 
-            const secondTypeDescrip = $(exercise).find(`.${typeDescriptors[1]}`).first().val();
-            if (secondTypeDescrip && secondTypeDescrip.trim().length > 0 && secondTypeDescrip > 0) {
-                exerciseObj[`${typeDescriptors[1]}`] = $(exercise).find(`.${typeDescriptors[1]}`).first().val();
-            } else {
-                alertArr.push(`a valid number for ${typeDescriptors[1]}`);
+            if (typeDescriptors[1]) {
+                const secondTypeDescrip = $(exercise).find(`.${typeDescriptors[1]}`).first().val();
+                if (secondTypeDescrip.trim().length > 0 && secondTypeDescrip > 0) {
+                    exerciseObj[`${typeDescriptors[1]}`] = $(exercise).find(`.${typeDescriptors[1]}`).first().val();
+                } else {
+                    alertArr.push(`a valid number for ${typeDescriptors[1]}`);
+                }
             }
+
             if (typeDescriptors.includes('time')) {
                 const unitTime = $(exercise).find('.unitTime').first().val();
                 exerciseObj['unitTime'] = unitTime;
@@ -417,33 +422,28 @@ function createExercises(day) {
             exerciseObj['comments'] = comments.trim();
         }
 
-        console.log('exerciseObj: ', exerciseObj);
         exercisesArray.push(exerciseObj);
     });
 
     return exercisesArray;
 }
 
-// function saveProgram(program) {
-
-// fetch(`/programs/`,
-//     {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${bearer}`
-//         }, 
-//         body: JSON.stringify(program)
-//     })
-//     .then(res => {
-//         if (res.ok) {
-//             return window.location.replace('/profile.html');
-//         }
-//         else {
-//             throw Error(`Request rejected with status ${res.status}`);
-//         }
-//     })
-// }
-
-addProgLength();
-createProgramObj();
+function saveProgram(program) {
+    fetch(`/programs/`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${bearer}`
+            },
+            body: JSON.stringify(program)
+        })
+        .then(res => {
+            if (res.ok) {
+                return window.location.replace('/profile.html');
+            }
+            else {
+                throw Error(`Request rejected with status ${res.status}`);
+            }
+        })
+}

bdde50970b3da4ce98a67337393a3d064e7020c0
Alexis Louie <alouie1020@gmail.com>
2019-03-15 00:36:54 -0400

    added scroll effect

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,2 +1,9 @@
 const bearer = localStorage.getItem('authToken');
 const id = localStorage.getItem('userId');
+
+$('main').on('click', '.about-link', function(event){
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+      }, 200);
+})
